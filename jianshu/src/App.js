78befae3dce526e9c9108cf4d894a770cf0a036f
xf <3002862704@qq.com>
2020-01-16 16:30:29 +0800

    sss

--- jianshu/src/App.js
+++
@@ -1,9 +0,0 @@
-import React,{Component} from 'react';
-import TodoList from './components/TodoList'
-class App extends Component{
-  render(){
-    return <TodoList></TodoList>
-  }
-}
-
-export default App;
